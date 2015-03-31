c349517b5f50897f70c49e2a469ccd382d2bba9b
dharapanda123 <dhara.dhara.009@gmail.com>
2015-03-31 18:14:13 +1300

    understanding Razor

--- Shopping_Cart_Personal/Shopping_Cart_Personal/Controllers/ProductController.cs
+++ Shopping_Cart_Personal/Shopping_Cart_Personal/Controllers/ProductController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Shopping_Cart_Personal.Models;
 
 namespace Shopping_Cart_Personal.Controllers
 {
     public class ProductController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         // GET: Product
         public ActionResult Index()
         {
-            return View();
+            return View(db.Products.ToList());
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            return View(db.Products.Single(s=>s.Id == id));
         }
     }
 }
