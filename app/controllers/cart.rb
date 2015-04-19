4f17c1371dc36b25a09d3a4c0ad8897ed5fb1ad1
Cara Hill <chilly23@gmail.com>
2015-04-19 14:22:15 +1200

    Fixed up spacing and indentation.

--- app/controllers/cart.rb
+++ app/controllers/cart.rb
@@ -1,8 +1,10 @@
 enable :sessions
 
 before do
+
   @current_customer = Customer.find_by_id(session[:customer_id])
   @current_product = Product.find_by_id(params[:id])
+
 end
 
 get "/products/cart" do
@@ -19,22 +21,27 @@ end
 post "/add_to_cart" do
 
   order = Order.new(customer_id: @current_customer.id, product_id: @current_product.id, purchased: false)
+
   if order.save
     redirect '/products/cart'
   else
     session[:error] = "This item has not been saved to the cart."
     redirect back
   end
+
 end
 
 post '/buy_now' do
+
   Order.destroy_all
   session[:notice] = "You have purchased all items in your cart. Thank you for shopping with us."
   redirect '/products/cart'
+
 end
 
 post '/remove_from_cart' do
 
   order = Order.where(product_id: @current_product.id).destroy_all
   redirect back
+
 end
--- app/controllers/products.rb
+++ app/controllers/products.rb
@@ -1,32 +1,45 @@
 enable :sessions
 
 get '/' do
+
   @products = Product.all
+
   if @current_customer
     @cart_items = @current_customer.find_products
   end
   session[:notice] = nil
   erb :home_page
+
 end
 
 get '/products' do
+
   @products = Product.all
+
   if @current_customer
     @cart_items = @current_customer.find_products
   end
+
   session[:notice] = nil
   erb :home_page
+
 end
 
 get '/products/:id' do
+
   @current_product = Product.find_by_id(params[:id])
 
   if @current_customer
     @cart_items = @current_customer.find_products
   end
+
   erb :product
+
 end
 
 post '/products' do
+
   redirect('/products')
+
 end
+
